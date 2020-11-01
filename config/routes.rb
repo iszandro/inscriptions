a25408d0fb715610a5af9bf4ddd2920bdae93b9e
Isay Sosa <yasnet21@gmail.com>
2020-11-01 03:04:43 -0600

    Remove password routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :teachers, controllers: { registrations: :registrations }
+  devise_for :teachers, controllers: { registrations: :registrations }, skip: :passwords
 
   root to: 'courses#index'
 
