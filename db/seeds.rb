be27bd2bb3fa1d157c3408059c46410e6180a1e5
Isay Sosa <yasnet21@gmail.com>
2020-11-01 09:47:28 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+teacher = Teacher.create(first_name: 'Severus', last_name: 'Snape', email: 'severus@howarts.com',
+                         skills: %w[Magic Potions], password: :password, password_confirmation: :password)
+teacher.courses.create(name: 'Potions Basic 1', description: 'Basic course about potions in Howarts', status: :active)
+
+teacher = Teacher.create(first_name: 'Rolanda', last_name: 'Hooch', email: 'rolanda@howarts.com',
+                         skills: %w[Magic Fly Quidditch], password: :password, password_confirmation: :password)
+teacher.courses.create(name: 'Fly with Nimbus 2000', description: 'Basic course to fly with Nimbus 2000',
+                       status: :active)
